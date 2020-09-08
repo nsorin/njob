b144c225025675e71cd754ed89286858d72798a6
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2020-09-08 17:49:05 +0200

    Added auth server (not yet integrated)

---
+++ njob-auth/src/main/java/com/github/nsorin/njobauth/NjobAuthApplication.java
@@ -0,0 +1,13 @@
+package com.github.nsorin.njobauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjobAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjobAuthApplication.class, args);
+	}
+
+}
---
+++ njob-auth/src/test/java/com/github/nsorin/njobauth/NjobAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nsorin.njobauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjobAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

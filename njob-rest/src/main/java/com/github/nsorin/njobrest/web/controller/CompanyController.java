3d210594205cfef96717e42835c0f688107648bc
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2020-09-09 18:14:04 +0200

    Added security to rest service

--- njob-rest/src/main/java/com/github/nsorin/njobrest/web/controller/CompanyController.java
+++ njob-rest/src/main/java/com/github/nsorin/njobrest/web/controller/CompanyController.java
@@ -8,6 +8,7 @@ import com.github.nsorin.njobrest.repository.CompanyRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
